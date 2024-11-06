df1a07ff3bb7e87e08c4dd5a721d8a595dae11c4
Monnier Antoine <monnier.ant@free.fr>
2024-11-06 17:07:44 +0100

    fix: Publish Feature

--- publish.ts
+++ publish.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import * as fs from 'fs';
-import * as path from 'path';
+import axios from "axios";
+import * as fs from "fs";
+import * as path from "path";
 
 interface Compatibility {
   minimum: string;
@@ -14,53 +14,61 @@ interface Manifest {
 }
 
 async function readCompatibilityInfo(filePath: string): Promise<Compatibility> {
-  const data = fs.readFileSync(filePath, 'utf-8');
+  const data = fs.readFileSync(filePath, "utf-8");
   const manifest: Manifest = JSON.parse(data);
   return manifest.compatibility;
 }
 
 async function readModuleId(filePath: string): Promise<string> {
-  const data = fs.readFileSync(filePath, 'utf-8');
+  const data = fs.readFileSync(filePath, "utf-8");
   const manifest: Manifest = JSON.parse(data);
   return manifest.id;
 }
 
-async function updateReleaseVersion(githubUrl: string, version: string, authToken:string) {
-  const compatibilityInfo = await readCompatibilityInfo(path.resolve(__dirname, 'src' ,'system.json'));
-  const moduleId = await readModuleId(path.resolve(__dirname, 'src', 'system.json'));
+async function updateReleaseVersion(
+  githubUrl: string,
+  version: string,
+  authToken: string
+) {
+  const compatibilityInfo = await readCompatibilityInfo(
+    path.resolve(__dirname, "src", "system.json")
+  );
+  const moduleId = await readModuleId(
+    path.resolve(__dirname, "src", "system.json")
+  );
   try {
     const response = await axios.post(
-      'https://api.foundryvtt.com/_api/packages/release_version/',
+      "https://api.foundryvtt.com/_api/packages/release_version/",
       {
         id: moduleId,
         release: {
           version: version,
-          manifest: `${githubUrl}/issues/releases/download/${version}/system.json`,
+          manifest: `${githubUrl}/releases/download/v${version}/system.json`,
           notes: `${githubUrl}/releases/tag/${version}`,
           compatibility: {
             minimum: compatibilityInfo.minimum,
             verified: compatibilityInfo.verified,
-            maximum: compatibilityInfo.maximum
-          }
-        }
+            maximum: compatibilityInfo.maximum,
+          },
+        },
       },
       {
         headers: {
-          'Content-Type': 'application/json',
-          Authorization: authToken
-        }
+          "Content-Type": "application/json",
+          Authorization: authToken,
+        },
       }
     );
 
     console.log(response.data);
   } catch (error) {
-    console.error('Error updating release version:', error);
+    console.error("Error updating release version:", error);
     process.exit(1);
   }
 }
 
 updateReleaseVersion(
-  process.env.RELEASE_GITHUB_URL ?? "", 
+  process.env.RELEASE_GITHUB_URL ?? "",
   process.env.RELEASE_VERSION ?? "",
   process.env.RELEASE_AUTH_TOKEN ?? ""
 );
